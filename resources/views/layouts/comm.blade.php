4793f4d648913348babf1f7c76ebcf4c8665b665
xiaojie <648266313@qq.com>
2020-03-28 21:23:14 +0800

    完成用户注册功能

--- resources/views/layouts/comm.blade.php
+++ resources/views/layouts/comm.blade.php
@@ -10,8 +10,13 @@
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
+
+        @include('layouts._messages')
+
         @yield('content')
+        
         @include('layouts._footer')
+
       </div>
     </div>
   </body>
