10056ff8b600ad618e3f8a4ea94e4aee08ae2208
xiaojie <648266313@qq.com>
2020-03-28 22:23:52 +0800

    登录成功后导航逻辑

--- resources/views/layouts/comm.blade.php
+++ resources/views/layouts/comm.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Weibo App')</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
 
@@ -10,14 +10,12 @@
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
-
         @include('layouts._messages')
-
         @yield('content')
-        
         @include('layouts._footer')
-
       </div>
     </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
