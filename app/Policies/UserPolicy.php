a89dbd683204395a951c0015655d39bd3f81e9fe
xiaojie <648266313@qq.com>
2020-03-29 23:45:52 +0800

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -21,10 +21,15 @@ class UserPolicy
     /**
      * @param 第一个参数默认为当前登录用户实例
      * @param 第二个参数则为要进行授权的用户实例
-     * @return 如果 id 不相同的话，将抛出 403 异常信息来拒绝访问
+     * @return 如果 id 不相同的话，将抛出 403 异常信息来拒绝访问cl
      */
     public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
+    }
+    //只有当前用户拥有管理员权限且删除的用户不是自己时才显示链接。
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }
